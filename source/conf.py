00d6767aa169dde9fc837cd04c625e94c30a06a4
Julian <julian.handrup@appandweb.de>
2019-01-07 10:50:45 +0100

    update

--- source/conf.py
+++ source/conf.py
@@ -80,7 +80,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False
+}
+
+html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
